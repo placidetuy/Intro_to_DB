27657b088e291e47e62de3a5da1546394bf23af2
“Placide” <mugahiplacide@gmail.com>
2024-10-20 13:50:33 -0700

    Inserted sample data into alx_book_store tables

--- task_2.sql
+++ task_2.sql
@@ -1,11 +1,9 @@
--- Authors Table
-CREATE TABLE IF NOT EXISTS Authors (
+CREATE TABLE Authors (
     author_id INT NOT NULL AUTO_INCREMENT,
     author_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (author_id)
 );
 
--- Books Table
 CREATE TABLE Books (
     book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
@@ -14,10 +12,9 @@ CREATE TABLE Books (
     publication_date DATE DEFAULT NULL,
     PRIMARY KEY (book_id),
     KEY (author_id),
-    CONSTRAINT Books_ibfk_1 FOREIGN KEY (author_id) REFERENCES Authors (author_id)
+    CONSTRAINT Books_ibfk_1 FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Customers Table
 CREATE TABLE Customers (
     customer_id INT NOT NULL AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
@@ -26,17 +23,15 @@ CREATE TABLE Customers (
     PRIMARY KEY (customer_id)
 );
 
--- Orders Table
 CREATE TABLE Orders (
     order_id INT NOT NULL AUTO_INCREMENT,
     customer_id INT NOT NULL,
     order_date DATE NOT NULL,
     PRIMARY KEY (order_id),
     KEY (customer_id),
-    CONSTRAINT Orders_ibfk_1 FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+    CONSTRAINT Orders_ibfk_1 FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_Details Table
 CREATE TABLE Order_Details (
     order_detail_id INT NOT NULL AUTO_INCREMENT,
     order_id INT NOT NULL,
@@ -45,7 +40,7 @@ CREATE TABLE Order_Details (
     PRIMARY KEY (order_detail_id),
     KEY (order_id),
     KEY (book_id),
-    CONSTRAINT Order_Details_ibfk_1 FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    CONSTRAINT Order_Details_ibfk_2 FOREIGN KEY (book_id) REFERENCES Books (book_id)
+    CONSTRAINT Order_Details_ibfk_1 FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    CONSTRAINT Order_Details_ibfk_2 FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
 
