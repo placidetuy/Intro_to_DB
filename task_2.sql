495c67e4867c95d97d036c9e9bfb593ca7eb5279
“Placide” <mugahiplacide@gmail.com>
2024-10-20 13:09:14 -0700

    Inserted sample data into alx_book_store tables

--- task_2.sql
+++ task_2.sql
@@ -1,41 +1,51 @@
--- Create Authors Table
+-- Authors Table
 CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(100) NOT NULL
+    author_id INT NOT NULL AUTO_INCREMENT,
+    author_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (author_id)
 );
 
--- Create Books Table
-CREATE TABLE IF NOT EXISTS Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+-- Books Table
+CREATE TABLE Books (
+    book_id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
-    author_id INT,
+    author_id INT DEFAULT NULL,
     price DOUBLE NOT NULL,
     publication_date DATE DEFAULT NULL,
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    PRIMARY KEY (book_id),
+    KEY (author_id),
+    CONSTRAINT Books_ibfk_1 FOREIGN KEY (author_id) REFERENCES Authors (author_id)
 );
 
--- Create Customers Table
-CREATE TABLE IF NOT EXISTS Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+-- Customers Table
+CREATE TABLE Customers (
+    customer_id INT NOT NULL AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
-    address TEXT NOT NULL
+    address TEXT,
+    PRIMARY KEY (customer_id)
 );
 
--- Create Orders Table
-CREATE TABLE IF NOT EXISTS Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_id INT,
+-- Orders Table
+CREATE TABLE Orders (
+    order_id INT NOT NULL AUTO_INCREMENT,
+    customer_id INT NOT NULL,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    PRIMARY KEY (order_id),
+    KEY (customer_id),
+    CONSTRAINT Orders_ibfk_1 FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
 );
 
--- Create Order_Details Table
-CREATE TABLE IF NOT EXISTS Order_Details (
-    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
-    order_id INT,
-    book_id INT,
-    quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+-- Order_Details Table
+CREATE TABLE Order_Details (
+    order_detail_id INT NOT NULL AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    PRIMARY KEY (order_detail_id),
+    KEY (order_id),
+    KEY (book_id),
+    CONSTRAINT Order_Details_ibfk_1 FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+    CONSTRAINT Order_Details_ibfk_2 FOREIGN KEY (book_id) REFERENCES Books (book_id)
 );
+
