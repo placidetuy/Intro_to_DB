fbfe75cd1d3bf7a32ada6e6c08a94177cc8ae069
“Placide” <mugahiplacide@gmail.com>
2024-10-22 00:01:06 -0700

    Added new customer to the Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,16 +1,6 @@
 USE alx_book_store;
 
--- Update the existing entry
-UPDATE Customers 
-SET customer_name = 'Cole Baidoo', 
-    email = 'cbaidoo@sandtech.com', 
-    address = '123 Happiness Ave.'
-WHERE customer_id = 1;
-
--- Insert a new entry if customer_id = 1 does not exist
-INSERT INTO Customers (customer_id, customer_name, email, address)
-SELECT 1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'
-WHERE NOT EXISTS (
-    SELECT 1 FROM Customers WHERE customer_id = 1
-);
+-- Insert while ignoring duplicates
+INSERT IGNORE INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
 
