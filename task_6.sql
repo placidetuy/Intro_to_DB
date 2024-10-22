6b1f19c6988ca9c86d965c09e68eb7a1021946b3
“Placide” <mugahiplacide@gmail.com>
2024-10-22 03:29:22 -0700

    Fixed task 6: Insert multiple customers

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,23 @@
 USE alx_book_store;
 
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = VALUES(customer_name),
+email = VALUES(email),
+address = VALUES(address);
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = VALUES(customer_name),
+email = VALUES(email),
+address = VALUES(address);
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = VALUES(customer_name),
+email = VALUES(email),
+address = VALUES(address);
 
