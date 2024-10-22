81a2683ca0779b4a27c54a0b2e580811da0826c0
“Placide” <mugahiplacide@gmail.com>
2024-10-22 02:50:06 -0700

    Complete task 5: Inserted a single customer into the customer table

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,9 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
-ON DUPLICATE KEY UPDATE
-customer_name = 'Cole Baidoo',
-email = 'cbaidoo@sandtech.com',
-address = '123 Happiness Ave.';
+-- First, delete the existing customer entry with customer_id = 1
+DELETE FROM customer WHERE customer_id = 1;
+
+-- Then, insert the new customer data
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
