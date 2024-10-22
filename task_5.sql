b44dc63140bf04fe1bfd4f8c598018c560d1b02d
“Placide” <mugahiplacide@gmail.com>
2024-10-22 02:03:48 -0700

    Updated task_5.sql for inserting a new customer

--- task_5.sql
+++ task_5.sql
@@ -2,8 +2,19 @@ USE alx_book_store;
 
 INSERT INTO Customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
-ON DUPLICATE KEY UPDATE 
-    customer_name = 'Cole Baidoo',
-    email = 'cbaidoo@sandtech.com',
-    address = '123 Happiness Ave.';
+ON DUPLICATE KEY UPDATE
+customer_name = 'Cole Baidoo',
+email = 'cbaidoo@sandtech.com',
+address = '123 Happiness Ave.';
+
+
+
+USE alx_book_store;
+
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = 'Cole Baidoo',
+email = 'cbaidoo@sandtech.com',
+address = '123 Happiness Ave.';
 
